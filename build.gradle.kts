9a0bdc2eabc0ad88b524af49d5678f98f3e9aa72
ShrykeWindgrace <Shryke.windgrace@gmail.com>
2024-04-12 21:58:40 +0200

    Rewrite for new platform plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,41 +3,57 @@ fun environment(key: String) = providers.environmentVariable(key)
 
 plugins {
     id("java") // Java support
-    alias(libs.plugins.gradleIntelliJPlugin) // Gradle IntelliJ Plugin
+    alias(libs.plugins.gradleIntelliJPlatformPlugin) // Gradle IntelliJ Platform Plugin
 }
 
 group = properties("pluginGroup").get()
 version = properties("pluginVersion").get()
 
-// Configure project's dependencies
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
-// Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
+// Dependencies are managed with Gradle version catalog
+// read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
-//    implementation(libs.annotations)
+    intellijPlatform {
+        intellijIdeaUltimate("2024.1")
+        instrumentationTools()
+    }
 }
 
-// Configure Gradle IntelliJ Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    pluginName = properties("pluginName")
-    version = properties("platformVersion")
-    type = properties("platformType")
+// Configure Gradle IntelliJ Plugin
+// read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellijPlatform {
+    pluginConfiguration {
+        name = properties("pluginName")
+        version = properties("platformVersion")
+        id = properties("pluginId")
+        description = "Experimental support of Haskell via an LSP plugin"
 
-    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    plugins = properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
+        vendor {
+            name = "ShrykeWindgrace"
+            email = "shryke.windgrace@gmail.com"
+            url = "https://github.com/ShrykeWindgrace"
+        }
+        ideaVersion {
+            sinceBuild = properties("pluginSinceBuild")
+            untilBuild = properties("pluginUntilBuild")
+        }
+
+        // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
+//        plugins = properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
+    }
+    buildSearchableOptions = false
+    instrumentCode = true
 }
 
 tasks {
     wrapper {
         gradleVersion = properties("gradleVersion").get()
-    }
-
-    patchPluginXml {
-        version = properties("pluginVersion")
-        sinceBuild = properties("pluginSinceBuild")
-        untilBuild = properties("pluginUntilBuild")
     }
 }
 
